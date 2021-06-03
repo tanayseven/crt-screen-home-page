4ce04dad7cdad6dc0276bc9f8bc3d200a6b77302
Tanay PrabhuDesai <tanayseven@gmail.com>
2021-06-04 01:37:37 +0530

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import scss from 'rollup-plugin-scss';
+import typescript from '@rollup/plugin-typescript';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'src/main.ts',
+	output: {
+		file: 'public/bundle.js',
+		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		sourcemap: true
+	},
+	plugins: [
+		resolve(), // tells Rollup how to find date-fns in node_modules
+		commonjs(), // converts date-fns to ES modules
+		production && terser(), // minify, but only in production
+		scss({
+			output: 'public/bundle.css',
+			sass: require("sass")
+		}),
+		typescript({
+			sourceMap: true,
+		}),
+	]
+};
