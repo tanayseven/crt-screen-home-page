8ccd8cfaadcdc7fb5282b6c69aa3c22855f48c4c
Tanay PrabhuDesai <tanayseven@gmail.com>
2021-06-18 14:33:22 +0530

    Add basic termianl input, yet to improve it

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,46 @@
 import './reset.scss';
+import { cursor, prompt } from './terminalStrings';
 
+const entityMap = new Map<string, string>(Object.entries({
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    "/": '&#x2F;'
+}))
+
+export function escapeHtml(source: string) {
+    return String(source).replace(/[&<>"'\/]/g, (s: string) => entityMap.get(s)!);
+}
 
 class Computer {
-    readonly videoMemory: HTMLPreElement
-    constructor(videoMemory: HTMLPreElement) {
-        this.videoMemory = videoMemory;
-        this.videoMemory.innerHTML = `
-Tanay OS v1992 - Open source edition
-Feel free to type "help" to know all the commands
-Prompt><span class="blinking-cursor">█</span>
-hello
-        `;
+    terminalText = [
+        `Tanay OS v1992 - Open source edition`,
+        `Feel free to type "help" to know all the commands`,
+        `${prompt}`
+    ]
+    keyboardListener = (event: KeyboardEvent) => {
+        this.terminalText[this.terminalText.length - 1] += event.key;
+    };
+    constructor() {
+        document.addEventListener('keypress', this.keyboardListener);
+    }
+    render(screen: HTMLPreElement) {
+        const escapedTerminalText = this.terminalText.map(e => escapeHtml(e))
+        const textToBeRendered = escapedTerminalText.join('\n') + cursor;
+        if (screen.innerHTML === textToBeRendered)
+            return;
+        console.log(screen.innerHTML + '\n' + textToBeRendered);
+        screen.innerHTML = textToBeRendered;
     }
 }
 
 let computerObject: Computer = null;
 
+const screen = document.getElementById("video-memory") as HTMLPreElement;
 export const initializeTerminal = () => {
     console.log("Initializing...");
-    computerObject = new Computer(document.getElementById("video-memory") as HTMLPreElement);
+    computerObject = new Computer();
+    setInterval((): void => {
+        computerObject.render(screen);
+    }, 20);
 }
---
+++ src/terminalStrings.ts
@@ -0,0 +1,4 @@
+const prompt = "Prompt>"
+const cursor = `<span class="blinking-cursor">█</span>`
+
+export {prompt, cursor}
